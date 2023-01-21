0b82d5120405caf1204eac4aafe0540f5f4dc0ac
Mohamad <mohamadkassem101@gmail.com>
2023-01-21 21:58:48 +0200

    add hashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import MarketPage from "./components/Market/MarketPage";
 import AddFriends from "./components/AddFriends/AddFriends";
@@ -14,7 +14,7 @@ import ProtectRoute from "./components/protectRoute/protectRoute";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         {/* <Navbar /> */}
         <Routes>
           <Route exact path="/" element={<Authentication />} />
@@ -100,7 +100,7 @@ function App() {
             }
           />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
